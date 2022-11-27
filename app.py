87c2219c3a65e9c6b5eaeb0f162868a2ee078a39
sprout110 <sprout110@gmail.com>
2022-11-27 23:22:23 +0800

    adjust

--- app.py
+++ app.py
@@ -59,6 +59,6 @@ def handle_message(event):
     line_bot_api.reply_message(event.reply_token,message)
 
 ##主程式
-#import os
+import os
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=os.environ['PORT'])
