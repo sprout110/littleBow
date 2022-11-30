51b89a370f3b607daff3a275acd5b0270b044297
sprout110 <sprout110@gmail.com>
2022-11-30 13:34:14 +0800

    adjust app.py

--- app.py
+++ app.py
@@ -55,8 +55,8 @@ def handle_message(event):
         return 0
 
     elif re.match('s[0-9]{4}',usespeak): #顯示即時股價
-        #querystock = mystock.get_stock_realtime(usespeak[1:])
-        line_bot_api.push_message(uid, TextSendMessage(usespeak[1:]+'即時股價:'))
+        querystock = mystock.get_stock_realtime(usespeak[1:])
+        line_bot_api.push_message(uid, TextSendMessage(usespeak[1:]+'即時股價:'+querystock))
         return 0
 
     else:
