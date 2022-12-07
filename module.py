e81f02e1ce6baaca1179bfd0a5dfde6734e296f5
li-tun <sprout110@yahoo.com.tw>
2022-12-07 19:43:54 +0800

    msg do not conver to lower.

--- module.py
+++ module.py
@@ -5,12 +5,11 @@ from test import MyTest
 import re
 
 def UserSay(msg):
-    msg = msg.lower()
-    if re.match('s[0-9]{4}', msg):
+    if re.match('s[0-9]{4}', msg.lower()):
         return RealTime(msg)
-    elif re.match('k[0-9]{4}', msg):
+    elif re.match('k[0-9]{4}', msg.lower()):
         return KChart(msg)
-    elif re.match('t[0-9]{4}', msg):
+    elif re.match('t[0-9]{4}', msg.lower()):
         return MyTest(msg)
     else:
         return Echo(msg)
