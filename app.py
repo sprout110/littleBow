17502a67af3adf166a3d6c79cbaacbd376f97b35
li-tun <sprout110@yahoo.com.tw>
2022-12-07 14:56:46 +0800

    add requirements.txt

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, abort
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import *
-import mongodb
 import mystock
 import re
 from config import * 
