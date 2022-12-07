5be20741f19b3cb21000efa8cdeee835fcf89628
li-tun <sprout110@yahoo.com.tw>
2022-12-07 17:33:05 +0800

    adjust module.py

--- module.py
+++ module.py
@@ -1,11 +1,12 @@
 from echo import Echo
 from realtime import RealTime
 from kchart import KChart
+import re
 
 def UserSay(msg):
-    if msg == 's2412':
+    if re.match('s[0-9]{4}', msg):
         return RealTime(msg)
-    elif msg == 'k2412':
+    elif re.match('k[0-9]{4}',msg):
         return KChart(msg)
     else:
         return Echo(msg)
